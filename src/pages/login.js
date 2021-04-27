ac4687bb17d10e637883ef650ba0e706c00e560a
Neha Nikhat <nehanikhat18@gmail.com>
2021-04-28 02:28:39 +0530

    Setup Tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,22 @@
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router";
+import FirebaseContext from "../context/firebase";
+
 export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
+
   return <p>This is Login page</p>;
 }
