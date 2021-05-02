5993b935af18fd05aa60dc0a82f4683aca2af135
Neha Nikhat <nehanikhat18@gmail.com>
2021-05-02 21:48:01 +0530

    Added photos of following to the timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,20 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+
 export default function Timeline() {
+  const { photos } = usePhotos();
+
+  console.log("photos", photos);
+
   return (
     <div className="container col-span-2">
-      <p>This is the Timeline</p>
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   );
 }
