9ae48ae14664c2851ddaa5034858af4c1e1c6785
Neha Nikhat <nehanikhat18@gmail.com>
2021-05-02 22:42:57 +0530

    Added post image, actions and footer

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,12 +1,25 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
+import Image from "./image";
+import Actions from "./actions";
+import Footer from "./footer";
 
 export default function Post({ content }) {
-  console.log("content", content);
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
     </div>
   );
 }
